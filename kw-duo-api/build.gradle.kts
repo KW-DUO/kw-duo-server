d2b0345df1b19b709f0f0ce6a07cca812c80af15
Octoping <mooner925@gmail.com>
2024-04-17 16:04:37 +0900

    feat: Swagger 설치

--- kw-duo-api/build.gradle.kts
+++ kw-duo-api/build.gradle.kts
@@ -9,6 +9,7 @@ tasks.getByName("jar") {
 dependencies {
     implementation(project(":domain"))
     implementation(project(":support:logging"))
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
     runtimeOnly(project(":storage:db"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
