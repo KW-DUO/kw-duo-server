682dc828164397b0bf46daf5dc9f5cad9d0de6f0
Octoping <mooner925@gmail.com>
2024-04-17 16:07:57 +0900

    feat: CORS 설정

---
+++ kw-duo-api/src/main/kotlin/kwduo/config/WebConfig.kt
@@ -0,0 +1,32 @@
+package kwduo.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig(
+) : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns(
+                "http://localhost:*",
+                "https://localhost:*",
+                "https://*.vercel.app"
+            )
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.PATCH.name()
+            )
+            .allowCredentials(true)
+    }
+
+//    override fun addInterceptors(registry: InterceptorRegistry) {
+//        registry.addInterceptor(needLoginInterceptor)
+//            .addPathPatterns("/**")
+//    }
+}
---
+++ kw-duo-api/src/main/kotlin/kwduo/interceptor/NeedLoginInterceptor.kt
@@ -0,0 +1,35 @@
+package kwduo.interceptor
+//
+//import jakarta.servlet.http.HttpServletRequest
+//import jakarta.servlet.http.HttpServletResponse
+//import org.springframework.stereotype.Component
+//import org.springframework.web.method.HandlerMethod
+//import org.springframework.web.servlet.HandlerInterceptor
+//
+//@Component
+//class NeedLoginInterceptor(
+//    private val tokenProvider: TokenProvider
+//) : HandlerInterceptor {
+//    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+//        if (!hasNeedLoginAnnotation(handler)) {
+//            return true
+//        }
+//
+//        if (!hasValidAccessToken(request)) {
+//            throw UserNotAuthorizedException()
+//        }
+//
+//        return true
+//    }
+//
+//    private fun hasValidAccessToken(request: HttpServletRequest): Boolean {
+//        return getAccessCookie(request)
+//            ?.let { tokenProvider.validateToken(it) }
+//            ?: false
+//    }
+//
+//    private fun hasNeedLoginAnnotation(handler: Any): Boolean {
+//        return handler is HandlerMethod &&
+//                handler.method.isAnnotationPresent(NeedLogin::class.java)
+//    }
+//}
