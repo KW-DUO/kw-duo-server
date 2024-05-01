e065849f16ce4a28d4c589872881f2f47575ec3a
Octoping <mooner925@gmail.com>
2024-05-02 01:12:40 +0900

    feat: jwt 모듈 추가

--- kw-duo-api/build.gradle.kts
+++ kw-duo-api/build.gradle.kts
@@ -9,8 +9,12 @@ tasks.getByName("jar") {
 dependencies {
     implementation(project(":domain"))
     implementation(project(":support:logging"))
+    implementation(project(":support:jwt"))
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly(project(":storage:db"))
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     testImplementation("org.springframework.security:spring-security-test")
