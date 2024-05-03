07d61bba3f143206f85d9219b9619d07ed6c005e
Octoping <mooner925@gmail.com>
2024-05-03 10:51:34 +0900

    fix: 회원 가입 api에서 bio와 joinAt 제거

--- kw-duo-api/src/main/kotlin/kwduo/member/dto/MemberJoinRequestDTO.kt
+++ kw-duo-api/src/main/kotlin/kwduo/member/dto/MemberJoinRequestDTO.kt
@@ -1,34 +1,28 @@
 package kwduo.member.dto
 
 import member.dto.MemberJoinRequest
-import java.time.LocalDateTime
 
 data class MemberJoinRequestDTO(
     val oAuthId: String,
     val profileImgId: Long?,
     val nickname: String,
-    var bio: String,
     val department: String,
     val techStack: List<String>,
     var position: String,
     val email: String,
-    var githubUrl: String?,
-    var baekjoonId: String?,
-    val joinAt: LocalDateTime,
+    val githubUrl: String?,
+    val baekjoonId: String?,
 ) {
-    fun toRequest(): MemberJoinRequest {
-        return MemberJoinRequest(
+    fun toRequest() =
+        MemberJoinRequest(
             oAuthId = oAuthId,
             profileImgId = profileImgId,
             nickname = nickname,
-            bio = bio,
             department = department,
             techStack = techStack,
             position = position,
             email = email,
             githubUrl = githubUrl,
             baekjoonId = baekjoonId,
-            joinAt = joinAt,
         )
-    }
 }
