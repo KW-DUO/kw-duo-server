c88a65fc083acd164a092aefc92a3e173b37b429
Octoping <mooner925@gmail.com>
2024-05-29 17:54:08 +0900

    feat: 구글 oauth 서비스 구현

---
+++ kw-duo-api/src/main/kotlin/kwduo/auth/GoogleOAuthController.kt
@@ -0,0 +1,31 @@
+package kwduo.auth
+
+import com.fasterxml.jackson.databind.JsonNode
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GoogleOAuthController(
+    private val googleOAuthService: GoogleOAuthService,
+) {
+    @Operation(summary = "구글 로그인")
+    @GetMapping("/auth/google")
+    fun googleLogin(
+        @RequestParam code: String,
+    ): String {
+        // 구글에 api 쏴서 코드 검증
+        val accessToken = googleOAuthService.getAccessToken(code)
+
+        // 받은 액세스 토큰으로 유저 정보 확인
+        val userResource = googleOAuthService.getUserResource(accessToken)
+
+        // 유저 정보의 oAuthId만 리턴
+        return getOAuthId(userResource)
+    }
+
+    private fun getOAuthId(userResource: JsonNode): String {
+        return userResource["id"].asText()
+    }
+}
